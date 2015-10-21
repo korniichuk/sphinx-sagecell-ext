47dd186f9da775c388af45f1cc136a7128201ae3
Ruslan Korniichuk <ruslan.korniichuk@gmail.com>
2015-10-21 22:51:30 +0200

    [sagecell.py] Add 'SageCell' class

--- sagecell/sagecell.py
+++ sagecell/sagecell.py
@@ -6,3 +6,13 @@ from sphinx.util.compat import Directive
 
 class sagecell(General, Element):
     pass
+
+class SageCell(Directive):
+
+    has_content = True
+    required_arguments = 0
+    optional_arguments = 1
+    final_argument_whitespace = False
+    option_spec = {
+        "linked": directives.unchanged
+    }
